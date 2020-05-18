10f043e96410a7e07d4bb420c1dbaebf827e01a8
xinzhang <xinchang@greatonce.com>
2020-05-18 10:54:18 +0800

    项目初始化

---
+++ src/main/java/top/xinzhang0618/springboot/demo/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package top.xinzhang0618.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootDemoApplication.class, args);
+  }
+
+}
